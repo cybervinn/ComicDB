5302beb94ff8035cb944f6b5215d8c0ee330fec5
Vinson Lee <vinson.lee.ny@gmail.com>
2015-10-04 19:00:53 -0400

    Added User's New/Create page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,23 @@ class UsersController < ApplicationController
   def show
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Profile has been created"
+      redirect_to root_path
+    else
+      render 'new' #error: goes back to new page
+    end
+  end
+
   def edit
+    @user = User.find(id)
   end
 
   def update
@@ -15,5 +31,14 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:fname,
+                                   :lname,
+                                   :email,
+                                   :password)
+    end
 
 end
